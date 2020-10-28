3cfe86af24b92c27e754c6a0f2f1a2e6ba3d779d
plane <1358929314@qq.com>
2020-10-28 22:38:42 +0800

    Update ReactElement.js
    createElement 注释

--- packages/react/src/ReactElement.js
+++ packages/react/src/ReactElement.js
@@ -347,17 +347,22 @@ export function jsxDEV(type, config, maybeKey, source, self) {
  * See https://reactjs.org/docs/react-api.html#createelement
  */
 export function createElement(type, config, children) {
+  // propName 变量用于储存后面需要用到的元素属性
   let propName;
 
   // Reserved names are extracted
+  // props 变量用于储存元素属性的键值对集合
   const props = {};
 
+  // key、ref、self、source 均为 React 元素的属性
   let key = null;
   let ref = null;
   let self = null;
   let source = null;
 
+  // config 对象中存储的是元素的属性
   if (config != null) {
+    // 进来之后做的第一件事，是依次对 ref、key、self 和 source 属性赋值
     if (hasValidRef(config)) {
       ref = config.ref;
 
@@ -365,6 +370,7 @@ export function createElement(type, config, children) {
         warnIfStringRefCannotBeAutoConverted(config);
       }
     }
+    // 此处将 key 值字符串化
     if (hasValidKey(config)) {
       key = '' + config.key;
     }
@@ -372,7 +378,9 @@ export function createElement(type, config, children) {
     self = config.__self === undefined ? null : config.__self;
     source = config.__source === undefined ? null : config.__source;
     // Remaining properties are added to a new props object
+    // 接着就是要把 config 里面的属性都一个一个挪到 props 这个之前声明好的对象里面
     for (propName in config) {
+      // 筛选出可以提进 props 对象里的属性
       if (
         hasOwnProperty.call(config, propName) &&
         !RESERVED_PROPS.hasOwnProperty(propName)
@@ -384,10 +392,13 @@ export function createElement(type, config, children) {
 
   // Children can be more than one argument, and those are transferred onto
   // the newly allocated props object.
+  // childrenLength 指的是当前元素的子元素的个数，减去的 2 是 type 和 config 两个参数占用的长度
   const childrenLength = arguments.length - 2;
+  // 如果抛去type和config，就只剩下一个参数，一般意味着文本节点出现了
   if (childrenLength === 1) {
     props.children = children;
   } else if (childrenLength > 1) {
+    // 处理嵌套多个子元素的情况
     const childArray = Array(childrenLength);
     for (let i = 0; i < childrenLength; i++) {
       childArray[i] = arguments[i + 2];
@@ -401,6 +412,7 @@ export function createElement(type, config, children) {
   }
 
   // Resolve default props
+  // 处理 defaultProps
   if (type && type.defaultProps) {
     const defaultProps = type.defaultProps;
     for (propName in defaultProps) {
@@ -423,6 +435,7 @@ export function createElement(type, config, children) {
       }
     }
   }
+  // 最后返回一个调用ReactElement执行方法，并传入刚才处理过的参数
   return ReactElement(
     type,
     key,
